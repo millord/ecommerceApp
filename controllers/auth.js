dedf01c309c4fa4e3e5c51cd77ad73f74f562d88
Juan Millord <jmillord@ambiente.gob.do>
2020-01-10 15:54:35 -0400

    fixed err issue

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,7 +26,7 @@ exports.signin = (req, res) => {
   User.findOne({ email }, (err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        err: "User with that email does not exist. Please sign up"
+        error: "User with that email does not exist. Please sign up"
       });
     }
     // if user is found make sure the email and password match
